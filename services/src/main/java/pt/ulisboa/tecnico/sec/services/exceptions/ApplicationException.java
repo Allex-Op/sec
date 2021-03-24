ae349d710c337f736162eeae8b45ef9bdad1ae3a
BFreitas16 <b.d.a.freitas16@gmail.com>
2021-03-24 01:27:50 +0000

    Created application and invalid report exception

---
+++ services/src/main/java/pt/ulisboa/tecnico/sec/services/exceptions/ApplicationException.java
@@ -0,0 +1,34 @@
+package pt.ulisboa.tecnico.sec.services.exceptions;
+
+/**
+ * The top level application exception.
+ */
+public class ApplicationException extends Exception {
+	
+	/**
+	 * The serial version id (generated automatically by Eclipse)
+	 */
+	private static final long serialVersionUID = -3416001628323171383L;
+
+	
+	/**
+	 * Creates an exception given an error message
+	 * 
+	 * @param message The error message
+	 */
+	public ApplicationException(String message) {
+		super (message);
+	}
+	
+	
+	/**
+	 * Creates an exception wrapping a lower level exception.
+	 * 
+	 * @param message The error message
+	 * @param e The wrapped exception.
+	 */
+	public ApplicationException(String message, Exception e) {
+		super (message, e);
+	}
+
+}
