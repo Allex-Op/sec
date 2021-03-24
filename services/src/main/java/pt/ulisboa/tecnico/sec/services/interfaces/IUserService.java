1115b0aa3d1d646c03f56c68fb3779eb10f6cc88
BFreitas16 <b.d.a.freitas16@gmail.com>
2021-03-24 01:28:48 +0000

    updated user service interface to throw application exception

--- services/src/main/java/pt/ulisboa/tecnico/sec/services/interfaces/IUserService.java
+++ services/src/main/java/pt/ulisboa/tecnico/sec/services/interfaces/IUserService.java
@@ -1,9 +1,11 @@
 package pt.ulisboa.tecnico.sec.services.interfaces;
 
+import pt.ulisboa.tecnico.sec.services.exceptions.ApplicationException;
+
 public interface IUserService {
 	
-	public void submitLocationReport(String userID, int epoch, String report);
-	
 	public String obtainLocationReport(String userID, int epoch);
+	
+	public void submitLocationReport(String userID, int epoch, String report) throws ApplicationException;
 
 }
