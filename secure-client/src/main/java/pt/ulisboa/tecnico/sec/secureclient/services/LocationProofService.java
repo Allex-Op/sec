08efc5200b8be1d727cdac04e3914e0fe28738eb
Bruno Freitas <38732994+BFreitas16@users.noreply.github.com>
2021-04-13 21:48:53 +0100

    Verification of Nonces and digital signatures in response

--- secure-client/src/main/java/pt/ulisboa/tecnico/sec/secureclient/services/LocationProofService.java
+++ secure-client/src/main/java/pt/ulisboa/tecnico/sec/secureclient/services/LocationProofService.java
@@ -9,6 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import java.util.ArrayList;
+import java.util.List;
 
 import pt.ulisboa.tecnico.sec.secureclient.ClientApplication;
 import pt.ulisboa.tecnico.sec.services.dto.ClientResponseDTO;
@@ -19,24 +21,40 @@ import pt.ulisboa.tecnico.sec.services.interfaces.ILocationProofService;
 @Service
 public class LocationProofService implements ILocationProofService {
 	
+	private static List<String> nonces = new ArrayList<>();
+	
 	private static RestTemplate restTemplate = new RestTemplate();
 
 	@Override
 	public ProofDTO requestLocationProof(String url, RequestProofDTO request) {
 		HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        
-        HttpEntity<RequestProofDTO> entity = new HttpEntity<>(request, headers);
-        
-        ResponseEntity<ClientResponseDTO> result = restTemplate.exchange(url, HttpMethod.POST, entity, ClientResponseDTO.class);
-        ClientResponseDTO clientResponse = result.getBody();
+		
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
-        if(clientResponse != null && clientResponse.getErr() != null) {
+		HttpEntity<RequestProofDTO> entity = new HttpEntity<>(request, headers);
+
+		ResponseEntity<ClientResponseDTO> result = restTemplate.exchange(url, HttpMethod.POST, entity, ClientResponseDTO.class);
+		ClientResponseDTO clientResponse = result.getBody();
+
+		if(clientResponse != null && clientResponse.getErr() != null) {
 			System.out.println("[Client " + ClientApplication.userId + "] Error occurred asking for proof: " + clientResponse.getErr().getDescription());
 			return null;
 		}
 
-        return clientResponse.getProof();
+		// Replay attack verification
+		if(nonces.contains(clientResponse.getNonce())) {
+			System.out.println("[Client " + ClientApplication.userId + "] Error occurred asking for proof: Replay Attack - nonce repeated.");
+			return null;
+		}
+		nonces.add(clientResponse.getNonce());
+		
+		// Verify check digital signature
+		if (!CryptoService.checkDigitalSignature(CryptoService.buildClientResponseMessage(clientResponse), clientResponse.getDigitalSignature(), CryptoUtils.getClientPublicKey(clientResponse.getProof().getUserID()))) {
+			System.out.println("Can't validate request proof signature.");
+			return null;
+		}
+		
+		return clientResponse.getProof();
 	}
 
 }
