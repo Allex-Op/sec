119603587c8a3abe1d0e3c277e7c4a4b77d93cf6
Felix-Saraiva <sfelix222@gmail.com>
2021-04-14 17:26:13 +0100

    Updated Byzantine Quorum

--- services/src/main/java/pt/ulisboa/tecnico/sec/services/configs/ByzantineConfigurations.java
+++ services/src/main/java/pt/ulisboa/tecnico/sec/services/configs/ByzantineConfigurations.java
@@ -3,6 +3,6 @@ package pt.ulisboa.tecnico.sec.services.configs;
 public class ByzantineConfigurations {
     public static final int RANGE = 1; // Grid Range
     public static final int MAX_BYZANTINE_USERS = 1;
-    public static final int REQUIRES_NON_BYZ_USERS = MAX_BYZANTINE_USERS + 1;
-    public static final int MINIMUM_BYZ_QUORUM = 3; // > (N + f) / 2
+    public static final int NUMBER_OF_USERS = (3 * MAX_BYZANTINE_USERS) + 1;
+    public static final int MINIMUM_BYZ_QUORUM = (2 * MAX_BYZANTINE_USERS) + 1;
 }
